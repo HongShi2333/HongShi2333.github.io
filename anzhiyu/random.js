9e5014c0c816a13e81d5ffb245de0f6c8758d8d3
HongShi2333 <1986977075@qq.com>
2024-11-02 23:14:11 +0800

    Site updated: 2024-11-02 23:14:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/post/43128.html","/post/59887.html","/post/14780.html","/post/36894.html","/post/33825.html","/post/36809.html"];function toRandomPost(){
+var posts=["/post/43128.html","/post/36894.html","/post/36809.html","/post/59887.html","/post/33825.html","/post/14780.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

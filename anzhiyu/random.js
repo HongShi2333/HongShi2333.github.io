00d84a0755d5bfd1c9b1a1815b663e25cbb46101
HongShi2333 <1986977075@qq.com>
2025-10-01 23:15:33 +0800

    Site updated: 2025-10-01 23:15:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/post/45382.html","/post/62072.html","/post/26312.html"];function toRandomPost(){
+var posts=["/post/42496.html","/post/62072.html","/post/45382.html","/post/26312.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
